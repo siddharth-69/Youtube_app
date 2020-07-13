fd4d5a0a5e946b285dacee433221a06369ce59b4
Rajat Dutta <rajat@Rajats-MacBook-Air.local>
2020-07-13 14:02:06 +0530

    Updates

---
+++ precache-manifest.eb1e9be4ac3bb1cf56c4a678f26e634f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1229e7d18ee843dbed7e1728fb98c6fb",
+    "url": "/Youtube_app/index.html"
+  },
+  {
+    "revision": "7f5298cd9cb5a53c16f4",
+    "url": "/Youtube_app/static/css/main.102a208e.chunk.css"
+  },
+  {
+    "revision": "6f0662bcb526518da5f9",
+    "url": "/Youtube_app/static/js/2.4cde1a03.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Youtube_app/static/js/2.4cde1a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5298cd9cb5a53c16f4",
+    "url": "/Youtube_app/static/js/main.d33f6147.chunk.js"
+  },
+  {
+    "revision": "4336154f71f0ed09da2a",
+    "url": "/Youtube_app/static/js/runtime-main.231edfe1.js"
+  }
+]);
